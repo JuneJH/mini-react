da30898ac0f60febe3d3ab934d0e8957474400c6
JuneJH <15310487388@163.com>
2022-03-16 22:54:10 +0800

    feat(*): 渲染原始标签

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import ReactDOM from './react-dom';
 import React from './react'
 
-ReactDOM.render(React.createElement("h1", null, "Hello, world!"), document.getElementById("root"));
+ReactDOM.render(React.createElement("h1", {style:"color:red"}, "Hello, world!",React.createElement("span",{style:"color:yellow"},"hi children",React.createElement("div",{style:"color:#000"},"hi,mini-react"))), document.getElementById("root"));
--- src/react-dom/ReactDOMLegacy.ts
+++ src/react-dom/ReactDOMLegacy.ts
@@ -1,27 +1,42 @@
-function render(vdom:any,container:any){
+function render(vdom: any, container: any) {
     const node = createNode(vdom);
     container.appendChild(node);
 
 }
 
-function createNode(vdom:any){
-    const {type} = vdom;
-    if(typeof type === "string"){
+function createNode(vdom: any) {
+    const { type } = vdom;
+    if (typeof type === "string") {
         const node = updateHostComponent(vdom);
         return node;
     }
 }
 
-function updateHostComponent(vdom:any){
-    const {type,children} = vdom;
+function updateHostComponent(vdom: any) {
+    const { type, children,props } = vdom;
+    console.log(children)
     const node = document.createElement(type);
-    if(children.length === 1){
-        const text = document.createTextNode(children[0]);
-        node.appendChild(text);
-    }
+    updateProps(node,props);
+    reconcileChildren(children, node);
     return node;
 }
 
+function updateProps(node:any,props:any){
+    Object.keys(props).forEach((key:string)=>{
+        node.setAttribute(key,props[key]);
+    })
+}
+
+function reconcileChildren(children: any, node: any) {
+    children.forEach((child: any) => {
+        if(typeof child !== "object"){
+            const text = document.createTextNode(child);
+            node.appendChild(text);
+        }else{
+            render(child,node);
+        }
+    })
+}
 export {
     render
 }
