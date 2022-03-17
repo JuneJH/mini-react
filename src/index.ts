c5d41722b65b89f6dbd72f1bfa805293cc93a8b5
JuneJH <15310487388@163.com>
2022-03-17 23:00:09 +0800

    feat(*): 使用

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import ReactDOM from './react-dom';
 import React from './react'
 
-ReactDOM.render(React.createElement("h1", {style:"color:red"}, "Hello, world!",React.createElement("span",{style:"color:yellow"},"hi children",React.createElement("div",{style:"color:#000"},"hi,mini-react"))), document.getElementById("root"));
+ReactDOM.render(React.createElement("div", {style:"color:red"}, "Hello, world!",React.createElement("span",{style:"color:yellow"},"hi children",React.createElement("a",{style:"color:#000"},"hi,mini-react"))), document.getElementById("root"));
